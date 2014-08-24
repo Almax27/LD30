b701b82c6b08ce8d1e7a93925ee227ed9be4f21b
Graehu (Graham Hughes) <Graehu@gmail.com>
2014-08-24 14:03:43 +0100

    FireParticleText
    Fires the particle text with the text supplied.

--- Assets/scripts/TextParticle.cs
+++ Assets/scripts/TextParticle.cs
@@ -2,16 +2,28 @@
 using System.Collections;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(TextMesh))]
 public class TextParticle : MonoBehaviour
 {
   private Animator animator;
+  private TextMesh textMesh;
+
   void Start()
   {
     animator = gameObject.GetComponent<Animator>();
+    textMesh = gameObject.GetComponent<TextMesh>();
+
+
   }
   [ContextMenu("PositiveGrowth")]
   public void PositiveGrowth()
   {
+    animator.SetTrigger("PositiveGrowth");
+  }
+
+  public void FireParticleText(string text)
+  {
+    textMesh.text = text;
     animator.SetTrigger("PositiveGrowth");
   }
 }
