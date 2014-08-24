efc29cbbd46cb5c835e8e2bde6296628ad1ba269
Graehu (Graham Hughes) <Graehu@gmail.com>
2014-08-24 13:33:05 +0100

    Added Text Particle
    It's not actually a particle, it's just text that does a little animation.

---
+++ Assets/scripts/TextParticle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class TextParticle : MonoBehaviour
+{
+  private Animator animator;
+  void Start()
+  {
+    animator = gameObject.GetComponent<Animator>();
+  }
+  [ContextMenu("PositiveGrowth")]
+  public void PositiveGrowth()
+  {
+    animator.SetTrigger("PositiveGrowth");
+  }
+}
